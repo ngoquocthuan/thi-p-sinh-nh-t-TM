501b01acd35ad3420ae08444b31c2913ab021fc5
ngoquocthuan <ngoquocthuan257@gmail.com>
2022-04-15 18:34:02 +0700

    finsh-project

---
+++ config.js
@@ -0,0 +1,9 @@
+const nameGirl = 'Em';
+const giftUrl = 'http://nodemy.vn';
+const eventName = 'Chúc Mừng Sinh Nhật Em';
+const titleCard = 'Tặng THẢO MAI';
+const contentCard = 'Chúc THẢO MAI của anh sinh nhật tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với em trong hôm nay và cả những ngày sau';
+
+const giftImage = 'hot-girl.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
