dba94f0e169b8cb41aad5d2b7a2e788a33714ef3
ngoquocthuan <ngoquocthuan257@gmail.com>
2022-04-18 18:47:45 +0700

    điền nội dung commit vào đây

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 const nameGirl = 'Em';
 const giftUrl = 'http://nodemy.vn';
-const eventName = 'Chúc Mừng Sinh Nhật Em';
+const eventName = 'Chúc Mừng Sinh Nhật Thảo Mai';
 const titleCard = 'Tặng THẢO MAI';
-const contentCard = 'Chúc THẢO MAI của anh sinh nhật tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với em trong hôm nay và cả những ngày sau';
+const contentCard = 'Chúc THẢO MAI sinh nhật tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với em trong hôm nay và cả những ngày sau';
 
 const giftImage = 'hot-girl.png';
 const base64 = '';
